618a6c8d7cd1e29ca1eb35a7fcd929a1c69df6a4
mynameisbryce <bryce@handsome.is>
2013-10-25 11:56:22 -0500

    Formatting

--- javascripts/loader.jquery.js
+++ javascripts/loader.jquery.js
@@ -1,61 +1,61 @@
 $(document).ready(function(){
 
-		init = function(){
-			goLoader();
+	init = function(){
+		goLoader();
+		resizeHandler();
+		uiEffects();
+		$(window).resize(function(){
 			resizeHandler();
-			uiEffects();
-			$(window).resize(function(){
-				resizeHandler();
-			});
-		}
+		});
+	}
 
-		goLoader = function(){
-			loaderFunc();
-			loadAnimate = setInterval(loaderFunc, 7200);
-		}
+	goLoader = function(){
+		loaderFunc();
+		loadAnimate = setInterval(loaderFunc, 7200);
+	}
 
-		loaderFunc = function(){
-			var delay = 1200;
-			$('div.triangle').addClass('flip');
-			$('h1').addClass('activate');
+	loaderFunc = function(){
+		var delay = 1200;
+		$('div.triangle').addClass('flip');
+		$('h1').addClass('activate');
+		setTimeout(function(){
+			$('div.triangle').removeClass('flip');
 			setTimeout(function(){
-				$('div.triangle').removeClass('flip');
+				$('div.triangle').addClass('step2');
+				$('div.triangle').addClass('flip');
 				setTimeout(function(){
-					$('div.triangle').addClass('step2');
-					$('div.triangle').addClass('flip');
+					$('div.triangle').removeClass('step2').removeClass('flip');
 					setTimeout(function(){
-						$('div.triangle').removeClass('step2').removeClass('flip');
+						$('div.triangle').addClass('step3');
+						$('div.triangle').addClass('flip');
 						setTimeout(function(){
-							$('div.triangle').addClass('step3');
-							$('div.triangle').addClass('flip');
-							setTimeout(function(){
-								$('div.triangle').removeClass('step3').removeClass('flip');
-							}, delay);
+							$('div.triangle').removeClass('step3').removeClass('flip');
 						}, delay);
 					}, delay);
-				},delay);
-			}, delay);
-		}
+				}, delay);
+			},delay);
+		}, delay);
+	}
 
-		stopLoader = function(){
-			$('div.logo-container').addClass('disappear');
-			$('div.loader-container').addClass('disappear');
-			clearInterval(loadAnimate);
-		}
+	stopLoader = function(){
+		$('div.logo-container').addClass('disappear');
+		$('div.loader-container').addClass('disappear');
+		clearInterval(loadAnimate);
+	}
 
-		uiEffects = function(){
-			$('body').click(function(){
-					stopLoader();
-			});
-		}
+	uiEffects = function(){
+		$('body').click(function(){
+				stopLoader();
+		});
+	}
 
-		resizeHandler = function(){
-			var viewHeight = $(window).height();
-			var viewWidth = $(window).width();
-			$('div.loader-container').css('height',viewHeight);
-			$('div.loader-container').css('width', viewWidth);
-		}
+	resizeHandler = function(){
+		var viewHeight = $(window).height();
+		var viewWidth = $(window).width();
+		$('div.loader-container').css('height',viewHeight);
+		$('div.loader-container').css('width', viewWidth);
+	}
 
-		init();
+	init();
 		
 });
